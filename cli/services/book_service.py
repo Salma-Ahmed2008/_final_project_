26d7bf4ed0a2c626cd3e1bdfdd57528d370150fb
Salma-Ahmed2008 <salma.ahmed.ntss@gmail.com>
2026-06-30 02:54:16 +0300

    try

--- cli/services/book_service.py
+++ cli/services/book_service.py
@@ -1,6 +1,6 @@
 import requests
 
-base_url = "finalproject-production-66d0.up.railway.app"
+base_url = "http://127.0.0.1:8000"
 
 def get_all_books():
     response = requests.get(f"{base_url}/books")
