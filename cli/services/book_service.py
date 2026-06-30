39f291d5b181ba40e5aaefd9eb5d87b4fa4eedba
Salma-Ahmed2008 <salma.ahmed.ntss@gmail.com>
2026-06-30 03:05:44 +0300

    commit

--- cli/services/book_service.py
+++ cli/services/book_service.py
@@ -1,6 +1,6 @@
 import requests
 
-base_url = "http://127.0.0.1:8000"
+base_url = "https://finalproject-production-3ec5.up.railway.app"
 
 def get_all_books():
     response = requests.get(f"{base_url}/books")
